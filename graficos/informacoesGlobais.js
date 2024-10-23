eb7a2c478926a5f94186c30c73bf5e28d421ef87
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2024-10-23 19:58:13 -0300

    alt

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,8 +6,7 @@ async function vizualizarInformacoesGlobais() {
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que no mundo há um total de ${dados.total_pessoas_mundo} pessoas,onde desse total a grande maioria sendo ${dados.total_pessoas_com-acesso_educacao} tem acesso a educação ,o tempo médio para estudo  é de ${dados.tempo_medio_dias_estudando} horas por dia. Porém desse total de pessoas com educação superior é de ${dados.total_pessoas_com_educacao_superior} ?
-                A educação é o caminho para atingir o sucesso que você almeja.`
+    paragrafo.innerHTML = `Você sabia que no mundo há um total de ${dados.total_pessoas_mundo} pessoas,onde desse total a grande maioria sendo ${dados.total_pessoas_com-acesso_educacao} tem acesso a educação ,o tempo médio para estudo  é de ${dados.tempo_medio_dias_estudando} horas por dia. Porém desse total de pessoas com educação superior é de ${dados.total_pessoas_com_educacao_superior}? A educação é o caminho para atingir o sucesso que você almeja.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
